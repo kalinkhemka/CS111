3d04f1d45a1ecf931319b134b4340f947aed65c2
Kalin Khemka <kalin@kalin-cs111.(none)>
2015-03-11 03:44:23 -0700

    sigh

--- lab4/osppeer.c
+++ lab4/osppeer.c
@@ -580,7 +580,8 @@ static void task_download(task_t *t, task_t *tracker_task)
 		}
 
 		//Exercise 2B - Check to make sure our max file size isn't passed
-		if (t->total_written > MAXFILESIZ){
+		if (t->total_written > MAXFILESIZ)
+		{
 			error("ERROR: File has exceeded max size allowed. Retrying with new peer.");
 			goto try_again;
 		}
