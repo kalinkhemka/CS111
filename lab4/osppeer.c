cf4d12a98292dd5da6e76b709c94758b920d3474
Kalin Khemka <kalin@kalin-cs111.(none)>
2015-03-13 15:37:09 -0700

    final

--- lab4/osppeer.c
+++ lab4/osppeer.c
@@ -523,7 +523,7 @@ task_t *start_download(task_t *tracker_task, const char *filename)
 				message("* Got checksum '%s' for file '%s'\n", tracker_task->digest, filename);
 			}
 		} else {
-			message("Receiving the checksum failed!!!!!");
+			message("* No MD5 checksum was received.");
 		}
 	}
 	
@@ -759,7 +759,7 @@ static void task_upload(task_t *t)
 
 	//Exercise 2B - Check filename size
 	if (strlen(t->buf) > FILENAMESIZ){
-		error("ERROR: Filename is too large.");
+		error("* ERROR: Filename is too large.");
 		goto exit;
 	}
 
@@ -767,15 +767,15 @@ static void task_upload(task_t *t)
 	char path[PATH_MAX];
 	char work_dir[PATH_MAX];
 	if (!getcwd(work_dir, PATH_MAX)){
-		error("ERROR: Invalid working directory.");
+		error("* ERROR: Invalid working directory.");
 		goto exit;
 	}
 	if (!realpath(t->filename, path)){
-		error("ERROR: Invalid file path.");
+		error("* ERROR: Invalid file path.");
 		goto exit;
 	}
 	if (strncmp(work_dir, path, strlen(work_dir))){
-		error("ERROR: %s not located within working directory.", t->filename);
+		error("* ERROR: %s not located within working directory.", t->filename);
 		goto exit;
 	}
 	//End 2B Code
