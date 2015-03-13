49ed914a11a991285eb250033b3d8b631462e2c7
Kalin Khemka <kalin@kalin-cs111.(none)>
2015-03-13 02:21:47 -0700

    onemd5

--- lab4/osppeer.c
+++ lab4/osppeer.c
@@ -506,9 +506,6 @@ task_t *start_download(task_t *tracker_task, const char *filename)
 	peer_t *p;
 	size_t messagepos;
 	assert(tracker_task->type == TASK_TRACKER);
-
-	//if (evil_mode)
-	//	filename = "";
 	
 	message("* Finding peers for '%s'\n", filename);
 	
